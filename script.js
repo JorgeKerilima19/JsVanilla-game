5b49445934f43a0674d0a11df52c6d2a122f4cc4
JorgeMioska <quispechilojorge@gmail.com>
2022-11-21 07:11:30 -0500

    PTME ANIMACIONES DE

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const array1=["❤","😁","🌹","💖","😜","❤","😉","💖"]
+const array1=["❤","😁","🌹","💖","😜",,"😉"]
 
-
+const array2=array1.concat(array1)
 
 const $table=document.querySelector("#table");
 $table.innerHTML=""
-console.log($table);
-array1.forEach(element => {
+array2.forEach(element => {
     card=document.createElement("div");
-    card.textContent=element
+    cardContent=document.createElement("div")
+    cardContent.textContent=element
     card.classList.add("card")
+    cardContent.classList.add("card-content")
     $table.appendChild(card)
+    card.appendChild(cardContent)
 });
+
+const $cardContent=document.querySelectorAll(".card");
+$cardContent.forEach(el=>{
+    el.addEventListener("click",()=>{
+        console.log("X");
+        el.classList.toggle("frontwards");
+    });
+})
