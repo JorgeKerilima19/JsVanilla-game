ee87d90f795b842f47cef023a11bc5a2eeaf06d6
JorgeMioska <quispechilojorge@gmail.com>
2022-11-24 21:52:19 -0500

    js ready to be deleted :'v

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const array1=["❤","😁","🌹","💖","😜","😃","😉"]
 const array2=array1.concat(array1)
 
 const $table=document.querySelector("#table");
+$table.innerHTML=""
 function shufflingCards(){
     let result= array2.sort(function(){
         return 0.5 -Math.random();
@@ -10,7 +11,25 @@ function shufflingCards(){
     return result;
 }
 const sortedCards=shufflingCards();
-$table.innerHTML=""
+
+class memoryCards{
+    constructor(arr,card1,card2){
+        this.arr=arr
+        this.card1=card1;
+        this.card2=card2;
+    }
+    choosingCards(){
+        this.card1.toggle(".frontwards")
+    }
+    rightPair(){
+        $result.textContent="Nice!"
+    }
+    wrongGuess(){
+        $result.textContent="You missed it !"
+    }
+}
+const newGame=memoryCards(sortedCards,)
+
 sortedCards.forEach(element => {
     card=document.createElement("div");
     card.dataset.content=element
@@ -21,6 +40,7 @@ sortedCards.forEach(element => {
     $table.appendChild(card)
     card.appendChild(cardContent)
 });
+const $result=document.querySelector(".result h4")
 
 const $cardContent=document.querySelectorAll(".card");
 $cardContent.forEach(el=>{
@@ -33,14 +53,11 @@ $cardContent.forEach(el=>{
         const cardsToCompare=document.querySelectorAll(".frontwards")
         if (cardsToCompare.length>1){
             if (cardsToCompare[0].dataset.content===cardsToCompare[1].dataset.content){
-                console.log("NICE")
+                $result.textContent="Nice!"
             }else{
-                console.log("ERROR")
+                $result.textContent="You missed it !"
             }
         }
     });
 });
 
-results=document.createElement("fragment")
-
-document.body.appendChild(results)
