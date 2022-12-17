0e8e01d66430703d5d5f7b4de329b4fe6aaecf5c
JorgeMioska <quispechilojorge@gmail.com>
2022-12-17 03:34:21 -0500

    table display + moves cheacker

--- script.js
+++ script.js
@@ -1,77 +1,68 @@
-const array1=["❤","😁","🌹","💖","😜","😃","😉"]
-const array2=array1.concat(array1)
+const emojiArray=["💖","🌹","😎"];
+const contentToLoad=emojiArray.concat(emojiArray);
 
 const $table=document.querySelector("#table");
 $table.innerHTML=""
-function shufflingCards(){
-    let result= array2.sort(function(){
-        return 0.5 -Math.random();
-    });
-    return result;
-}
-const sortedCards=shufflingCards();
-sortedCards.forEach(element => {
-    card=document.createElement("div");
-    cardContent=document.createElement("div");
-    cardContent.textContent=element;
-    card.classList.add("card");
-    card.dataset.content=element
-    cardContent.classList.add("card-content");
-    $table.appendChild(card)
-    card.appendChild(cardContent)
-});
 
-class newGame{
-    constructor(time, cards){
-        this.time=time;
-        this.cards=cards;
-        this.minutesController=document.querySelector(".minutes");
-        this.secondsController=document.querySelector(".seconds");
+function displayTable(){
+    contentToLoad.forEach(element => {
+        card=document.createElement("div");
+        cardContent=document.createElement("div");
+        cardContent.textContent=element;
+        card.classList.add("card");
+        card.dataset.content=element
+        cardContent.classList.add("card-content");
+        $table.appendChild(card)
+        card.appendChild(cardContent)
+    })
+}
+
+class memoryGame{
+    constructor(array){
+        this.array=array
+        this.totalMoves=0;
         this.movesController=document.querySelector(".moves");
     }
     startGame(){
-        this.cardToCheck=null;
-        this.minutesController=0;
-        this.secondsController=0;
-        this.movesController=0;
-        this.cardsToCompare=[];
-        this.busy=true;
+        this.totalMoves=0;
     }
-    // startCounting(){
-    //     let seconds=10;
-    //     let minutes=0;
-    //     let secondsText;
-    //     let minutesText;
-    //     let chronometer;
-    //     function updateChronometer(){
-    //         seconds--;
-    //         if (seconds < 0){
-    //             seconds=59;
-    //             minutes--;
-    //         }
-    //         if(minutes<0){
-    //             seconds=0;
-    //             minutes=0;
-    //             clearInterval(chronometer)
-    //         }
-    //         secondsText=seconds;
-    //         minutesText=minutes;
-    //         seconds<10? secondsText="0"+seconds:false
-    //         minutes<10? minutesText="0"+minutes:false
-    //         document.querySelector(".seconds").innerText=secondsText
-    //         document.querySelector(".minutes").innerText=minutesText
-    //     }
-    //     chronometer=setInterval(updateChronometer, 1000);
-    // }
-    // movesUpdate(){
-    //     this.moves++;
-    //     let textMoves;
-    //     textMoves=this.moves 
-    //     this.moves<10? textMoves="0"+this.moves:false;
-    //     document.querySelector(".moves").innerText=textMoves;
-    // }
-
+    flipCards(card){
+        const pairOfCards=document.querySelectorAll(".frontwards");
+        if(pairOfCards.length>1){
+            return
+        }
+        card.classList.add("frontwards")
+        this.totalMoves++;
+        this.movesController.innerText=this.totalMoves;
+    }
+    ableToFlip(){
+    }
 }
+
+function gameReady(){
+    const cards=document.querySelectorAll(".card");
+    const newGame =new memoryGame(contentToLoad);
+    cards.forEach(card=>{
+        card.addEventListener("click",()=>{
+            newGame.flipCards(card)
+        });
+    });
+}
+
+
+if(document.readyState==="loading"){
+    document.addEventListener("DOMContentLoaded",displayTable())
+}else{
+    displayTable();
+    gameReady();
+}
+
+
+
+
+
+
+
 
 
 
