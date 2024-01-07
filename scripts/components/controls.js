100998a112c29d4a2692475c2063d807a1c92063
JorgeMioska <quispechilojorge@gmail.com>
2024-01-07 07:47:16 -0500

    mute control working+styles

--- scripts/components/controls.js
+++ scripts/components/controls.js
@@ -3,9 +3,16 @@ import audioPlayer from "../audioClass.js";
 export const controls = () => {
   const container = document.createElement("div");
 
+  const muteContainer = document.createElement("div");
   const muteButton = document.createElement("input");
+  const muteLabel = document.createElement("label");
+  const muteImg = new Image(25, 25);
 
   muteButton.type = "checkbox";
+  muteButton.id = "mute";
+  muteImg.src = "./assets/mute.svg";
+
+  muteLabel.htmlFor = "mute";
 
   muteButton.addEventListener("change", () => {
     if (muteButton.checked) {
@@ -15,7 +22,15 @@ export const controls = () => {
     audioPlayer.bgSound.volume = 0.6;
   });
 
-  container.appendChild(muteButton);
+  muteLabel.appendChild(muteImg);
+
+  muteContainer.appendChild(muteButton);
+  muteContainer.appendChild(muteLabel);
+
+  container.appendChild(muteContainer);
+
+  //classes
+  muteContainer.classList.add("controls-mute");
 
   return container;
 };
